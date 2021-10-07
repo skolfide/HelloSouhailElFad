0c693c7250a6af4cc18804c02cf5089ec0455acd
linux <souhailelfad@gmail.com>
2021-10-07 04:20:00 +0200

    aa

---
+++ app/src/main/java/com/example/hellosouhailelfad/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.hellosouhailelfad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        /*Log.e("MainActivity", "I'm working");
+
+        Log.e("MainActivity", "Application cannot be launched");*/
+
+
+    }
+}
